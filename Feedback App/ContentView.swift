67ca696de0e9b7da2ba2d3abc915182ad7e757f1
VedantPatle0305 <vedantpatle594@gmail.com>
2025-09-19 12:55:43 +0530

    Initial Commit

---
+++ Feedback App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Feedback App
+//
+//  Created by Vedant Patle on 19/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Feedback App/Feedback_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Feedback_AppApp.swift
+//  Feedback App
+//
+//  Created by Vedant Patle on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Feedback_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
